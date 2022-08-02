555cca70dc3eb595642d5c259988b12cd6ad3b8e
Efsane <efs.gun@gmail.com>
2022-08-02 17:31:45 +0300

    day 3

--- 03_Day_Booleans_operators_date/03_day_starter/exercises/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/exercises/main.js
@@ -20,4 +20,30 @@ const string1="python"
 const string2="jargon"
 console.log(string1.length !== string2.length)
 
+//EXERCİSES7
 
+const now=new Date()
+
+console.log(now.getFullYear()) //2022
+
+console.log(now.getMonth()+1) // 7
+
+console.log(now) //Sun Jul 31 2022 12:35:29 GMT+0300 (GMT+03:00)
+
+console.log(now.getDate()) //31
+
+console.log(now.getHours()) //12
+
+console.log(now.getMinutes()) //35
+
+console.log(now.getTime()/1000)
+
+//EXERCİSES Seviye 2
+const base=prompt(" üçgenin tabanını giriniz")
+const height=prompt(" üçgenin yüksekliğini giriniz")
+ const area=base*height/2
+
+const areaElemet=document.getElementById("hesap")
+areaElemet.innerHTML=area
+
+console.log(document.getElementById("deneme").value) //EXERCİSES8
